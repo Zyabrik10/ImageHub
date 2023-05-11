01a95a1061718eca161e997c38ad2bd825286e1a
Zyabrik10 <sasamazurok@gmail.com>
2023-05-11 21:13:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,13 +19,17 @@ async function addImage() {
     const response = await fetchImages(query, page);
     const { data } = response;
 
-    gallery_box.innerHTML += createImageList(data.hits);
+    const links = createImageList(data.hits);
+
+    gallery_box.insertAdjacentHTML('beforeend', links);
     preventItemsClick('.gallery__link');
-  
+
     gallery.refresh();
-  
+
     if (data.totalHits <= 40 * (page - 1) + data.hits.length) {
-      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
       load_more_button.style.display = 'none';
     }
 
@@ -39,33 +43,36 @@ async function addImage() {
 async function searchImage(event) {
   event.preventDefault();
 
-  try{
+  try {
     const input_value = search_form_input.value.trim();
     const response = await fetchImages(input_value);
     const { data } = response;
-  
+
     if (data.hits.length === 0) {
-      Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
       return;
     }
-  
+
     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-  
+
     query = input_value;
     page = 1;
-  
+
     if (data.totalHits > 40) {
       page += 1;
       load_more_button.style.display = 'block';
     }
-  
-    gallery_box.innerHTML = createImageList(data.hits);
-  
+
+    const links = createImageList(data.hits);
+
+    gallery_box.innerHTML = links;
     preventItemsClick('.gallery__link');
 
     gallery = initSliderLightBox();
-  } catch(error){
-    Notiflix.Notify.failure("Oops! Something went wrong");
+  } catch (error) {
+    Notiflix.Notify.failure('Oops! Something went wrong');
     console.error(error);
   }
 }
