f88a9c7d96e93360b74f65ea59c7f539f5196906
Alex Mazurok | Мазурок Олександр | Zyabrik10 <Zyabrik10@gmail.com>
2024-08-23 14:04:52 +0200

    Revert "Finding issue"
    This reverts commit 337fadbd77b905dde250c6b444c28058cdf8fc05.

--- src/index.js
+++ src/index.js
@@ -4,61 +4,59 @@ import _ from "lodash";
 // Main function
 
 window.addEventListener("load", async () => {
-  console.log('App started');
+  try {
+    lightbox.init(".image-collection-list");
+    searchForm.init(".search-form");
+    searchForm.findAndRender(queryAPIConfig.query);
 
-  // try {
-  //   lightbox.init(".image-collection-list");
-  //   searchForm.init(".search-form");
-  //   searchForm.findAndRender(queryAPIConfig.query);
+    const anchor = document.querySelector(".anchor");
+    const imageColletionTop =
+          lightbox.lightboxContainer.container.getBoundingClientRect().top;
 
-  //   const anchor = document.querySelector(".anchor");
-  //   const imageColletionTop =
-  //         lightbox.lightboxContainer.container.getBoundingClientRect().top;
+    window.addEventListener(
+      "scroll",
+      _.throttle(async () => {
+        if (
+          window.scrollY + innerHeight >=
+          lightbox.lightboxContainer.container.scrollHeight
+        ) {
+          queryAPIConfig.page += 1;
 
-  //   window.addEventListener(
-  //     "scroll",
-  //     _.throttle(async () => {
-  //       if (
-  //         window.scrollY + innerHeight >=
-  //         lightbox.lightboxContainer.container.scrollHeight
-  //       ) {
-  //         queryAPIConfig.page += 1;
+          const response = await imageAPI.fetch({
+            query: queryAPIConfig.query,
+            page: queryAPIConfig.page,
+            orientation: "horizontal",
+            per_page: 20,
+            category: "nature",
+          });
 
-  //         const response = await imageAPI.fetch({
-  //           query: queryAPIConfig.query,
-  //           page: queryAPIConfig.page,
-  //           orientation: "horizontal",
-  //           per_page: 20,
-  //           category: "nature",
-  //         });
+          const images = response.data.hits.map(
+            ({ id, largeImageURL, tags }) => {
+              return {
+                url: largeImageURL,
+                largeUrl: largeImageURL,
+                description: tags,
+                id,
+              };
+            }
+          );
 
-  //         const images = response.data.hits.map(
-  //           ({ id, largeImageURL, tags }) => {
-  //             return {
-  //               url: largeImageURL,
-  //               largeUrl: largeImageURL,
-  //               description: tags,
-  //               id,
-  //             };
-  //           }
-  //         );
+          lightbox.addImages(images);
+        }
 
-  //         lightbox.addImages(images);
-  //       }
-
-  //       if (
-  //         window.scrollY + innerHeight >=
-  //         imageColletionTop + innerHeight * 1.5
-  //       ) {
-  //         !anchor.classList.contains("active") &&
-  //           anchor.classList.add("active");
-  //       } else {
-  //         anchor.classList.contains("active") &&
-  //           anchor.classList.remove("active");
-  //       }
-  //     }, 500)
-  //   );
-  // } catch (e) {
-  //   console.error(e);
-  // }
+        if (
+          window.scrollY + innerHeight >=
+          imageColletionTop + innerHeight * 1.5
+        ) {
+          !anchor.classList.contains("active") &&
+            anchor.classList.add("active");
+        } else {
+          anchor.classList.contains("active") &&
+            anchor.classList.remove("active");
+        }
+      }, 500)
+    );
+  } catch (e) {
+    console.error(e);
+  }
 });
